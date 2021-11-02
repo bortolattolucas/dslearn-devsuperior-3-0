8b1a5bcecd6105664340518b96085dffa9e98c2c
Lucas Bortolatto <lucas-bortolatto@outlook.com>
2021-11-02 18:19:01 -0300

    Custom content for current user

--- backend/src/main/java/tech/lucasbortolatto/dslearn/repositories/NotificationRepository.java
+++ backend/src/main/java/tech/lucasbortolatto/dslearn/repositories/NotificationRepository.java
@@ -1,7 +1,12 @@
 package tech.lucasbortolatto.dslearn.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tech.lucasbortolatto.dslearn.entities.Notification;
+import tech.lucasbortolatto.dslearn.entities.User;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    Page<Notification> findByUser(User user, Pageable pageable);
 }
---
+++ backend/src/main/java/tech/lucasbortolatto/dslearn/services/NotificationService.java
@@ -0,0 +1,28 @@
+package tech.lucasbortolatto.dslearn.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import tech.lucasbortolatto.dslearn.dto.NotificationDTO;
+import tech.lucasbortolatto.dslearn.entities.Notification;
+import tech.lucasbortolatto.dslearn.entities.User;
+import tech.lucasbortolatto.dslearn.repositories.NotificationRepository;
+
+@Service
+public class NotificationService {
+
+    @Autowired
+    private NotificationRepository notificationRepository;
+
+    @Autowired
+    private AuthService authService;
+
+    @Transactional(readOnly = true)
+    public Page<NotificationDTO> getNotificationsForCurrentUser(Pageable pageable) {
+        User user = authService.getAuthenticatedUser();
+        Page<Notification> page = notificationRepository.findByUser(user, pageable);
+        return page.map(NotificationDTO::new);
+    }
+}
