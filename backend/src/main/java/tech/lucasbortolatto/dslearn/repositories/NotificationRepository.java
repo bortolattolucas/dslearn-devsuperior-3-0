fd25b3a4a67ab155d9fd62e20800685540ebbb7b
Lucas Bortolatto <lucas-bortolatto@outlook.com>
2021-11-03 22:12:50 -0300

    Notifications custom query

--- backend/src/main/java/tech/lucasbortolatto/dslearn/repositories/NotificationRepository.java
+++ backend/src/main/java/tech/lucasbortolatto/dslearn/repositories/NotificationRepository.java
@@ -3,10 +3,15 @@ package tech.lucasbortolatto.dslearn.repositories;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import tech.lucasbortolatto.dslearn.entities.Notification;
 import tech.lucasbortolatto.dslearn.entities.User;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
-    Page<Notification> findByUser(User user, Pageable pageable);
+    @Query("SELECT obj FROM Notification obj WHERE "
+            + "(obj.user = :user) AND "
+            + "(:unreadOnly = false OR obj.read = false) " // macete pra trazer todas qdo variavel for falsa ou nao informada
+            + "ORDER BY obj.moment DESC")
+    Page<Notification> find(User user, boolean unreadOnly, Pageable pageable);
 }
--- backend/src/main/java/tech/lucasbortolatto/dslearn/resources/NotificationResource.java
+++ backend/src/main/java/tech/lucasbortolatto/dslearn/resources/NotificationResource.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import tech.lucasbortolatto.dslearn.dto.NotificationDTO;
 import tech.lucasbortolatto.dslearn.services.NotificationService;
@@ -18,7 +19,8 @@ public class NotificationResource {
     private NotificationService notificationService;
 
     @GetMapping
-    public ResponseEntity<Page<NotificationDTO>> findById(Pageable pageable) {
-        return ResponseEntity.ok(notificationService.getNotificationsForCurrentUser(pageable));
+    public ResponseEntity<Page<NotificationDTO>> findById(@RequestParam(name = "unreadOnly", defaultValue = "false") Boolean unreadOnly,
+                                                          Pageable pageable) {
+        return ResponseEntity.ok(notificationService.getNotificationsForCurrentUser(unreadOnly, pageable));
     }
 }
